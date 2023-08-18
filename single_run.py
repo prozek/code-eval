f3a28b9d1543e2fb4428ea190f1427afaa59d2f6
prozek <piotrrozek91@gmail.com>
2023-08-18 22:37:38 +0200

    parsing human eval problems in single_run

--- single_run.py
+++ single_run.py
@@ -5,13 +5,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import argparse
+import jsonlines 
 
 def single_run(problem_number, model):
-    problems = stream_jsonl('human_eval/data/HumanEval.jsonl.gz')
-    print(problems[0])
+    
+    problems = []
+
+    with jsonlines.open('human-eval/data/HumanEval.jsonl') as reader:
+        for obj in reader:
+            problems.append(obj)
+
+    print(len(problems))
+    print(problems[0].keys())
+
+    for key in problems[problem_number].keys():
+        print(key)
+        print(problems[problem_number][key])
+        print('\n ========== MARKER ========== \n')
 
 
-    print('your mom has ' + str(problem_number) + ' problems, but' + model + ' ain\'t one')
+    #print('your mom has ' + str(problem_number) + ' problems, but' + model + ' ain\'t one')
     pass
 
 single_run(1, "gpt2")
